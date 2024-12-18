4527b84b606d25e8d53150e180bf68c07b7145a6
miqo221 <miqonazarian@gmail.com>
2024-12-19 00:55:36 +0400

    left changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.scss";
 
 function App() {
-  return <div className="app">
-    <div className='container'>AA</div>
-  </div>;
+  return (
+    <div className="app">
+      <div className="container">AA</div>
+    </div>
+  );
 }
 
 export default App;
