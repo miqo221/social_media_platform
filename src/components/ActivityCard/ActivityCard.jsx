e7eb126376e55740f794dad9bced36e80b65fc3b
miqo221 <miqonazarian@gmail.com>
2024-12-20 01:25:05 +0400

    Created Activity Card component

---
+++ src/components/ActivityCard/ActivityCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import verificationIcon from "../../assets/images/icons/verificationIcon.png";
+
+import "./ActivityCard.scss";
+
+const ActivityCard = ({ userPic, userName, activityTime }) => {
+  return (
+    <div className="activityCard">
+      <div className="activityInfo">
+        <div className="imgDiv">
+          <img src={userPic} alt="Profile Picture" className="profilePic" />
+          <img
+            src={verificationIcon}
+            alt="Verification Icon"
+            className="verified"
+          />
+        </div>
+        <div className="userInfo">
+          <h6 className="userName">{userName}</h6>
+          <p className="activity">
+            Followed on you <span>{activityTime}</span>
+          </p>
+        </div>
+      </div>
+      <div className="buttonsDiv">
+        {/* toxum em es buttonery stex - minchev verjnakan button component unenanq*/}
+        <button className="remove">Remove</button>
+        <button className="follow">Follow Back</button>
+      </div>
+    </div>
+  );
+};
+
+export default ActivityCard;
