5a321ab0cb162e0705fb9fdef6debd2bdbd6b85a
miqo221 <miqonazarian@gmail.com>
2024-12-29 18:48:25 +0400

    Finalized Not Found Page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,30 @@
+import { Link } from "react-router-dom";
+import Button from "../../components/Button/Button";
+import { ROUTES } from "../../routes";
+import notFoundImg from "../../assets/images/png/pageNotFound.png";
+
 import "./NotFound.scss";
 
 export const NotFound = () => {
+  const button = () => {
+    console.log("asf");
+  };
+
   return (
-    <div>
-      <h1>Not Found</h1>
+    <div className="notFoundPage">
+      <Link to={ROUTES.HOME}>
+        <Button
+          content="Go To Login Page"
+          button_class="notFound_page_button"
+          button_function={button}
+          button_type="submit"
+          button_disabled={false}
+        />
+      </Link>
+      <div className="notFoundBox">
+        <h1>Page Not Found.</h1>
+        <img src={notFoundImg} alt="Not Found Image" />
+      </div>
     </div>
   );
 };
