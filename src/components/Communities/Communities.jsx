762a4d45efcee811dac429cd6978f7bc62d004b3
Davit <davit.petrosyan10@edu.ysu.am>
2025-01-05 21:40:01 +0400

    initial community section

---
+++ src/components/Communities/Communities.jsx
@@ -0,0 +1,19 @@
+import "./Communities.scss";
+const Communities = () => {
+  return (
+    <div className="communities-section">
+      <div className="communities-section-title">
+        <p>Communities</p>
+      </div>
+      <div className="communities-section-content">
+        <div className="asd"></div>
+        <div>
+          <p>Community Name</p>
+          <p>your friends are in</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Communities;
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,13 +1,10 @@
-import Skills from "../../components/Skills/Skills";
-import Modal from "../../components/Modal/Modal";
+import Communities from "../../components/Communities/Communities";
 import "./Profile.scss";
 
 export function Profile() {
   return (
     <div>
-      <p>Profile</p>
-      <Skills initialSkill={["human"]} />
-      {/* <Modal modal_p={"Sure to delete this skill ?"} /> */}
+      <Communities />
     </div>
   );
 }
